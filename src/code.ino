7f866932da3d9a455ef7f9064abac3d93f4e79dd
Andrei Zvonimir Crnkovic <crnkovic.a.z@gmail.com>
2015-01-31 20:46:10 +0100

    Added working example

--- src/code.ino
+++ src/code.ino
@@ -1,11 +1,60 @@
 #include <EtherCard.h>
+#include <dht.h>
+#define DHT11_PIN 5
 
-// ethernet interface ip address
+static uint32_t timer;
+dht DHT;
+
+static byte session;
+Stash stash;
+byte Ethernet::buffer[700];
+
 static byte myip[] = { 172,16,2,2 };
 static byte gwip[] = { 172,16,2,1 };
+static byte dnsip[] = { 172,16,0,3 };
 static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+const char website[] PROGMEM = "172.16.0.8";
+static byte api[] = { 172,16,1,25 };
 
-byte Ethernet::buffer[500];
+static void my_callback (byte status, word off, word len)
+{
+    Serial.println(">>>");
+    Ethernet::buffer[off+300] = 0;
+    Serial.print((const char*) Ethernet::buffer + off);
+    Serial.println("...");
+}
+
+static void sendData() {
+    Serial.println("Sending data...");
+
+    byte sd = stash.create();
+
+    stash.print("{\"token\": \"...\", \"GPSlat\": null, \"GPSlog\": null, \"GPSalt\": null, \"temp\": ");
+    stash.print(readTemp());
+    stash.print(", \"errorCode\": null }");
+    stash.save();
+    int stash_size = stash.size();
+
+    // Compose the http POST request, taking the headers below and appending
+    // previously created stash in the sd holder.
+    Stash::prepare(PSTR("POST http://$F/api/events HTTP/1.0" "\r\n"
+    "Host: $F" "\r\n"
+    "Content-Length: $D" "\r\n"
+    "Content-Type: application/json" "\r\n"
+    "\r\n"
+    "$H"),
+    website, website, stash_size, sd);
+
+    // send the packet - this also releases all stash buffers once done
+    // Save the session ID so we can watch for it in the main loop.
+    session = ether.tcpSend();
+}
+
+String readTemp()
+{
+    DHT.read11(DHT11_PIN);
+    return String(DHT.temperature, DEC);
+}
 
 void setup()
 {
@@ -15,16 +64,31 @@ void setup()
     while (!Serial) ;
 
     if (ether.begin(sizeof Ethernet::buffer, mymac) == 0)
-        Serial.println( "Failed to access Ethernet controller");
+        Serial.println(F("Failed to access Ethernet controller"));
 
-    ether.staticSetup(myip, gwip);
+    ether.staticSetup(myip, gwip, dnsip);
 
-    ether.printIp("IP:  ", ether.myip);
-    ether.printIp("GW:  ", ether.gwip);
-    ether.printIp("DNS: ", ether.dnsip);
+    ether.printIp("IP:", ether.myip);
+    ether.printIp("GW:", ether.gwip);
+    ether.printIp("DNS:", ether.dnsip);
+
+    if (!ether.dnsLookup(website))
+        Serial.println("DNS failed");
+
+    ether.copyIp(ether.hisip, api);
+    ether.hisport = 5000;
+
+    ether.printIp("SRV:", ether.hisip);
+    sendData();
 }
 
 void loop()
 {
-    // Send request to http://log.dokku.d.h/api/events
+    ether.packetLoop(ether.packetReceive());
+
+    const char* reply = ether.tcpReply(session);
+    if (reply != 0) {
+        Serial.println("Got a response!");
+        Serial.println(reply);
+    }
 }
